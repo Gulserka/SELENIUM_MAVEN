11ff3d7e17bdad7ab46a23d6558b75b29b1ffecb
Gulser Kayacan <gkayacan2014@gmail.com>
2023-01-06 02:10:04 +0300

    06/01/2023

--- src/test/java/a_erolhocapracticelesson/day02/Dropdown.java
+++ src/test/java/a_erolhocapracticelesson/day02/Dropdown.java
@@ -7,6 +7,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import utilities.TestBase;
 
+import java.util.List;
+
 public class Dropdown extends TestBase {
 
     @Test
@@ -15,11 +17,17 @@ public class Dropdown extends TestBase {
         driver.get("https://amazon.com");
 
         //Dropdown menuyu yazdıralım
-        WebElement dropdown = driver.findElement(By.xpath("//*[@id='searchDropdownBox']"));
-        Select select = new Select(dropdown);
-        for (WebElement w : select.getOptions()) {
-            System.out.println(w.getText());
-        }
+        //**** 1.YOL
+        List<WebElement> dropdown = driver.findElements(By.xpath("//*[@id='searchDropdownBox']"));
+        //Select select = new Select(dropdown);
+        dropdown.forEach(t-> System.out.println(t.getText()));
+
+        //**** 2.YOL
+//        WebElement dropdown = driver.findElement(By.xpath("//*[@id='searchDropdownBox']"));
+//        Select select = new Select(dropdown);
+//        for (WebElement w : select.getOptions()) {
+//            System.out.println(w.getText());
+//        }
 
 
         //Dropdown menuden baby secelim
