1d74ddc4bf0e3054a93eb1d98effb8143515da0f
Gulser Kayacan <gkayacan2014@gmail.com>
2023-01-10 16:54:25 +0300

    10/01/2023

--- src/test/java/day12/C02_ExplicitWait.java
+++ src/test/java/day12/C02_ExplicitWait.java
@@ -4,7 +4,11 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.TestBase;
+
+import java.time.Duration;
 
 public class C02_ExplicitWait extends TestBase {
 
@@ -13,18 +17,31 @@ public class C02_ExplicitWait extends TestBase {
     // Note: implicitlyWait Sayfa daki webelementlerin yüklenmesi için bekliyor ama içerde bir buttona basınca loading yani bekleme yaparken çalışmıyor
 
 
+
     @Test
     public void explicitWaitTest(){
-
-        //https://the-internet.herokuapp.com/dynamic_loading/1
+         /*
+      https://the-internet.herokuapp.com/dynamic_loading/1
+      Start buttonuna tıkla
+      Hello World! Yazının sitede oldugunu test et
+       */
         driver.get("https://the-internet.herokuapp.com/dynamic_loading/1");
+//        Start buttonuna tıkla
+        driver.findElement(By.xpath("//div[@id='start']//button")).click();
+//        WebElement helloWorldElement = driver.findElement(By.xpath("//div[@id='finish']//h4"));
+//        Assert.assertEquals("Hello World!",helloWorldElement.getText());
+//        IMPLICIT WAIT ILE TEST CASE CALISMADI.
+//        COZUM EXPLICIT WAITDE
 
-        //Start buttonuna tıkla
-        driver.findElement(By.xpath("//div[@id='start']/button")).click();
-        WebElement helloWorldElement = driver.findElement(By.xpath("//div[@id='finish']//h4"));
+//        1. WebDriverWait objesini olustur
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
-        //Hello World! Yazının sitede oldugunu test et
-        Assert.assertEquals("Hello World!",helloWorldElement.getText());
+//        2. wait objesini kullanarak bekleme problemini cozmeye calis
+        WebElement helloWorldElement=wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@id='finish']//h4")));
+        String helloWorldText = helloWorldElement.getText();
+        Assert.assertEquals("Hello World!",helloWorldText);
+
+
 
 
 
