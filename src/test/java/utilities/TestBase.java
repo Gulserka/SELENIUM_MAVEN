815e0081f14e629ddc773ad70962131017bc429b
Gulser Kayacan <gkayacan2014@gmail.com>
2023-01-16 01:57:06 +0300

    16/01/2023

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.apache.commons.io.FileUtils;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
@@ -216,6 +217,34 @@ public abstract class TestBase {
         String path = System.getProperty("user.dir")+"/test-output/Screenshots/"+currentTime+"image.png";
         FileUtils.copyFile(image,new File(path));
     }
+
+
+
+
+
+    public void selectFromDropdown(WebElement dropdown, String secenek){
+//        selectFromDropdown(driver.findElement(By.xpath("//select[@id='year']")), "2005"); -> year dan 2005'I sececek
+//        selectFromDropdown(driver.findElement(By.xpath("//select[@id='month']")), "January"); -> month January
+//        selectFromDropdown(driver.findElement(By.id("day")), "12"); -> Day 12
+//        Gonderilen dropdown elemention tum optionslari alinir
+
+
+        //option tagleri select tag'inin içerisinde olduğu için tagName Locaters kullanıyoruz
+        List<WebElement> options = dropdown.findElements(By.tagName("option"));//Tum option tagli elementleri aliyorum
+        for (WebElement eachOption : options){
+            if (eachOption.getText().equals(secenek)){
+                eachOption.click();
+                break;
+            }
+        }
+    }
+
+    @Test
+    public void selectFromDropdown(){
+        selectFromDropdown(driver.findElement(By.xpath("//select[@id='year']")), "2005");//2005 i secti
+    }
+
+
 
 
 }
