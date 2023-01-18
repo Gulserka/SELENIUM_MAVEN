eeb09614627629ee66040150ce6b94eebb13963b
Gulser Kayacan <gkayacan2014@gmail.com>
2023-01-18 21:07:55 +0300

    18/01/2023

---
+++ src/test/java/a_erolhocapracticelesson/LivedeSoruOlarakAtti.java
@@ -0,0 +1,13 @@
+package a_erolhocapracticelesson;
+
+public class LivedeSoruOlarakAtti {
+
+    ////https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html adresine gidelim
+    ////Hesap makinasından 2 basamaklı random iki tane sayı seçelim ve random 4 işlemden birini yaptıralım
+    ////Sonucu konsola yazdırıp
+    ////Sonucun doğrulamasını yapalım
+
+
+
+
+}
--- src/test/java/day16/C01_ExcelRead.java
+++ src/test/java/day16/C01_ExcelRead.java
@@ -9,7 +9,10 @@ public class C01_ExcelRead {
     @Test
     public void readExcelTest() throws Exception {
 //    WORKBOOK (EXCEL DOSYASI) > WORKSHEET/SHEET(SAYFA) >ROW (SATIR)> CELL(VERI HUCRESI)
-        String path ="./src/test/java/resources/Capitals.xlsx";
+        String path ="./src/test/java/resources/Capitals.xlsx"; //onune ./ yazmamiz gerekiyo, diger seyleri kapsasin diye
+
+
+
 //        String path =".\\src\\test\\java\\resources\\Capitals.xlsx";//WINDOWS
 //        DOSYAYI AC
         FileInputStream fileInputStream = new FileInputStream(path);
@@ -20,28 +23,38 @@ public class C01_ExcelRead {
         Sheet sheet1 = workbook.getSheet("Sheet1");// Sheet1 isimli sayfayi ac
 //        ILK SATIRA GIT / Row
         Row row1 = sheet1.getRow(0);//ILK SATIRA GIT
+
 //        INK SATIRDAKI ILK VERIYI AL
         Cell cell1 = row1.getCell(0);//ILK HUCREDEKI DATAYI VER
 //        O VERIYI YAZDIR
         System.out.println(cell1);
+
 //        1. SATIR 2. SUTUN
         Cell cell12 = sheet1.getRow(0).getCell(1);
         System.out.println(cell12);
 //        3.Satir 1.Sutun yazdir ve O verinin France oldugunu test et
-        String cell31 = sheet1.getRow(2).getCell(0).toString();
+        String cell31 = sheet1.getRow(2).getCell(0).toString(); //Assertion falan yaparken stringe cevirmemiz makul
         System.out.println(cell31);
         Assert.assertEquals("France",cell31);
+
+
 //       Exceldeki toplam kullanilan satir sayisini bul
         int toplamSatirSayisi = sheet1.getLastRowNum()+1;//son satir numarasi.index 0dan basliyor. sayma sayilari 1 den. +1 ekle
         System.out.println(toplamSatirSayisi);//11
+
+
 //        Kullanilan toplam satir sayisini bul
         int kullanilanToplamSatirSayisi = sheet1.getPhysicalNumberOfRows();
         System.out.println(kullanilanToplamSatirSayisi);//1 den basliyor
+
+
+
 //        COUNTRY, CAPITALS key ve value lari map a alip print et
 //        {{USA, D.C},{FRANCE, PARIS},...}
+
 //        Variable olusturalim. Bu variable exceldeki country,capital verilerini tutacak
         Map<String,String> ulkeBaskentleri = new HashMap<>();
-        for (int satirSayisi=1;satirSayisi<kullanilanToplamSatirSayisi;satirSayisi++){
+        for (int satirSayisi=1;satirSayisi<kullanilanToplamSatirSayisi;satirSayisi++){  //basliklari almak istemedigimiz icin 1den baslattik
             String country = sheet1.getRow(satirSayisi).getCell(0).toString();
             String capital = sheet1.getRow(satirSayisi).getCell(1).toString();
             ulkeBaskentleri.put(country,capital);
